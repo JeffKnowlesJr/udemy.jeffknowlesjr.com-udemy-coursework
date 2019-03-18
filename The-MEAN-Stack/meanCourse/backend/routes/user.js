7d1c7f73b83940b5730c7ee754789064fb1b56da
Jeff Knowles Jr <jeffrey.knowles@gmail.com>
2019-03-18 00:19:40 -0400

    Completed up through secure token to the frontend

--- The-MEAN-Stack/meanCourse/backend/routes/user.js
+++ The-MEAN-Stack/meanCourse/backend/routes/user.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-// const User = require('../models/user');
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash =>{
-      const user = new user({
+      const user = new User({
         email: req.body.email,
-        password: req.body.password
+        password: hash
       });
       user.save()
         .then(result => {
@@ -26,5 +27,40 @@ router.post('/signup', (req, res, next) => {
         });
     });
 });
+
+router.post('/login', (req, res, next) => {
+  let fetchedUser;
+  User.findOne({email: req.body.email})
+    .then(user => {
+      if(!user) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        })
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        });
+      }
+      const token = jwt.sign({
+          email: fetchedUser.email, userId: fetchedUser._id},
+          'secret_this_should_be_longer',
+          {expiresIn: "1h" }
+          ); // SECRET PASSWORD
+      res.status(200).json({
+        token: token
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(401).json({
+        message: 'Auth failed'
+      });
+    });
+})
 
 module.exports = router;
