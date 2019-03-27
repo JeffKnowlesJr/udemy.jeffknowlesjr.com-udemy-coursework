477c5dc38be98efa7264dc9b6ec00b939d5bc1b5
Jeff Knowles Jr <jeffrey.knowles@gmail.com>
2019-03-27 13:31:31 -0400

    Completed up through PHP arrays

--- Wordpress/fictional-university/app/public/wp-content/themes/fictional-university-theme/index.php
+++ Wordpress/fictional-university/app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,16 @@
-this is our amazing custom theme
+<?php 
+
+  $names = array(
+    'Jeff',
+    'Pete',
+    'Zach'
+  );
+
+  $i = 0;
+  while($i < count($names)) {
+    echo "<li>Hi, my name is $names[$i]</li>";
+    $i++;
+  }
+
+?>
+
